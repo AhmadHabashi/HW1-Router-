2722516580e5d68568e6b85f34a3332c12e5d145
Ahmad_Habashi <109224395+AhmadHabashi@users.noreply.github.com>
2025-12-29 15:11:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+//Ahmad Habashi - 214626913
+//Otman Egbaria  - 3................
+
+
+
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/users');
+const data = require('./data');
+
+const port = process.env.PORT || 3000;
+
+// Middleware for JSON processing (if required)
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routers
+app.use('/api/users', userRoutes); // All user routes
+
+// Main Page
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to the Main Page</h1>');
+});
+
+//Get  all Products
+app.get('/api/products', (req, res) => {
+  res.json(data.products);
+});
+
+//Get product by ID
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find(p => p.id === parseInt(req.params.id));
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
+//Add new product 
+app.post('/api/products', (req, res) => {
+  const newProduct = {
+    id:req.body.id,
+    name: req.body.name,
+    price: req.body.price,
+    stock: req.body.stock
+  };  
+  if (newProduct.price > 0 && newProduct.stock >=0) {
+    data.products.push(newProduct);
+    res.status(201).json(newProduct);
+  } else {
+    res.status(400).json({ message: 'Invalid product data' });
+  }
+});
+
+//uppdate the product by ID 
+app.put('/api/products/:id', (req, res) => {
+  const product = data.products.find(p => p.id === parseInt(req.params.id));
+  if (product) {
+    const updatedPrice = req.body.price;
+    const updatedStock = req.body.stock;
+    if (updatedPrice > 0 && updatedStock >=0) {
+      product.price = updatedPrice;
+      product.stock = updatedStock;
+      res.json(product);
+    } else 
+      {
+      res.status(400).json({ message: 'Invalid product data' });
+    }
+  }
+   else  {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
+//Delete product by ID
+app.delete('/api/products/:id', (req, res) => {
+  const productIndex = data.products.findIndex(p => p.id === parseInt(req.params.id));
+  if (productIndex !== -1) {
+    const deletedProduct = data.products.splice(productIndex, 1);
+    res.json(deletedProduct);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  } 
+});
+
+// Processing route 404
+app.use((req, res) => {
+  res.status(404).send('<h1>404 Not Found</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
---
+++ data.js
@@ -0,0 +1,46 @@
+const data = {
+  products: [
+    { id: 1, name: 'Laptop', price: 1000 , 'Stock': 50 },
+    { id: 2, name: 'Phone', price: 500 , 'Stock': 150 },
+    { id: 3, name: 'Tablet', price: 300, 'Stock': 100 },
+    { id: 4, name: 'Headphones', price: 100 , 'Stock': 200 },
+    { id: 5, name: 'Monitor', price: 200 , 'Stock': 75 }
+  ],
+  users: [
+    {
+      "id": 1,
+      "name": "Alice Johnson",
+      "email": "alice.johnson@example.com",
+      "age": 25,
+      "city": "New York",
+      "isActive": true
+    },
+    {
+      "id": 2,
+      "name": "Bob Smith",
+      "email": "bob.smith@example.com",
+      "age": 30,
+      "city": "Los Angeles",
+      "isActive": false
+    },
+    {
+      "id": 3,
+      "name": "Charlie Brown",
+      "email": "charlie.brown@example.com",
+      "age": 35,
+      "city": "Chicago",
+      "isActive": true
+    },
+    {
+      "id": 4,
+      "name": "Diana Prince",
+      "email": "diana.prince@example.com",
+      "age": 28,
+      "city": "San Francisco",
+      "isActive": true
+    }
+  ]
+  
+} 
+  
+  module.exports = data;
